3614feaaf8aac4dea369d38d19e2b50ed30c2149
Nikolai <nikolai.kalsin@citronium.com>
2020-02-25 16:59:23 +0300

    add simple validator

--- lib/src/blocs/login_page.dart
+++ lib/src/blocs/login_page.dart
@@ -9,6 +9,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPage extends State<LoginPage> {
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,20 +18,32 @@ class _LoginPage extends State<LoginPage> {
         title: Text('Login page'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'This login page',
-            ),
-            RaisedButton(
-              child: Text('Posts page'),
-              onPressed: () {
-                // Navigate to the second screen using a named route.
-                Navigator.pushNamed(context, '/posts');
-              },
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter your email',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              RaisedButton(
+                child: Text('Posts page'),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Navigator.pushNamed(context, '/posts');
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
